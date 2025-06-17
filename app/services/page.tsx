287d66895d09e1667d49415a503b95c954ef5e1a
Roommetrics Developments <nico.roommetrics@gmail.com>
2025-06-17 12:13:09 +0200

    Update page.tsx

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useRef, useEffect, useState } from 'react'
+import React, { useRef, useEffect } from 'react'
 import { motion } from 'motion/react'
 import Link from 'next/link'
 
